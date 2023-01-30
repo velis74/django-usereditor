c66d98d0bf30deb9c943d7a697df5be2f8dd543c
Klemen Pukl <klemen.pukl@gmail.com>
2023-01-30 15:33:48 +0100

    Updated dynamicforms to 0.70

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('requirements.txt', 'r') as fh:
 
 setuptools.setup(
     name="django-usereditor",
-    version="0.0.7",
+    version="0.0.8",
     author="Klemen Pukl",
     author_email="klemen.pukl@velis.si",
     description="Aplication for user overview and editing",

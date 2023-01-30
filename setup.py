c656194e1949f6534fb4f1c96682e35b9da07096
Klemen Pukl <klemen.pukl@gmail.com>
2023-01-30 15:07:36 +0100

    Updated dynamicforms to 0.50

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('requirements.txt', 'r') as fh:
 
 setuptools.setup(
     name="django-usereditor",
-    version="0.0.5",
+    version="0.0.6",
     author="Klemen Pukl",
     author_email="klemen.pukl@velis.si",
     description="Aplication for user overview and editing",
--- usereditor/rest.py
+++ usereditor/rest.py
@@ -3,9 +3,9 @@ from django.db import transaction
 from django.db.models.expressions import Value
 from django.db.models.functions import Coalesce, Concat
 from django.utils.translation import ugettext_lazy as _
-from dynamicforms import fields, serializers, viewsets
-from dynamicforms.action import Actions
-from dynamicforms.mixins import DisplayMode
+from dynamicforms_legacy import fields, serializers, viewsets
+from dynamicforms_legacy.action import Actions
+from dynamicforms_legacy.mixins import DisplayMode
 from rest_framework import routers
 from rest_framework.permissions import IsAdminUser
 from rest_framework.response import Response
